1fc0dbcae8855a7d1936035ad355f2ac8c7327b2
hrushi01 <hrushikesh.ambike@gmail.com>
2023-03-05 14:43:12 +0530

    -

--- src/components/Extra.jsx
+++ src/components/Extra.jsx
@@ -27,7 +27,8 @@ function Extra() {
           textAlign: "center",
           fontWeight: "bold",
           fontSize: "25px",
-        }}>
+        }}
+      >
         Testing WebCam Section
       </h1>
       <br />
@@ -41,32 +42,38 @@ function Extra() {
         </p>
         <button
           onClick={recordWebcam.open}
-          className="bg-gray-600 text-white rounded p-2 w-fit m-2 ml-0  pr-3 disabled:opacity-50">
+          className="bg-gray-600 text-white rounded p-2 w-fit m-2 ml-0  pr-3 disabled:opacity-50"
+        >
           Open camera
         </button>
         <button
           onClick={recordWebcam.start}
-          className="bg-gray-600 text-white rounded p-2 w-fit m-2 ml-0  pr-3 disabled:opacity-50">
+          className="bg-gray-600 text-white rounded p-2 w-fit m-2 ml-0  pr-3 disabled:opacity-50"
+        >
           Start recording
         </button>
         <button
           onClick={recordWebcam.stop}
-          className="bg-gray-600 text-white rounded p-2 w-fit m-2 ml-0  pr-3 disabled:opacity-50">
+          className="bg-gray-600 text-white rounded p-2 w-fit m-2 ml-0  pr-3 disabled:opacity-50"
+        >
           Stop recording
         </button>
         <button
           onClick={recordWebcam.retake}
-          className="bg-gray-600 text-white rounded p-2 w-fit m-2 ml-0  pr-3 disabled:opacity-50">
+          className="bg-gray-600 text-white rounded p-2 w-fit m-2 ml-0  pr-3 disabled:opacity-50"
+        >
           Retake recording
         </button>
         <button
           onClick={recordWebcam.download}
-          className="bg-gray-600 text-white rounded p-2 w-fit m-2 ml-0  pr-3 disabled:opacity-50">
+          className="bg-gray-600 text-white rounded p-2 w-fit m-2 ml-0  pr-3 disabled:opacity-50"
+        >
           Download recording
         </button>
         <button
           onClick={saveFile}
-          className="bg-gray-600 text-white rounded p-2 w-fit m-2 ml-0  pr-3 disabled:opacity-50">
+          className="bg-gray-600 text-white rounded p-2 w-fit m-2 ml-0  pr-3 disabled:opacity-50"
+        >
           Save file to server
         </button>
         <video
@@ -86,7 +93,8 @@ function Extra() {
           textAlign: "center",
           fontWeight: "bold",
           fontSize: "25px",
-        }}>
+        }}
+      >
         Testing Microphone and Speech-Converter Section
       </h1>
       <br />
