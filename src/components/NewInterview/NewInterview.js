2155374bdbf3251384dfb833d40c0ee2bb2a41e5
hrushi01 <hrushikesh.ambike@gmail.com>
2023-03-06 01:09:40 +0530

    NewInterview css added

--- src/components/NewInterview/NewInterview.js
+++ src/components/NewInterview/NewInterview.js
@@ -13,123 +13,232 @@ function NewInterview() {
 
   const handleSubmitInterview = {
     // fucntion
-  }; 
-  const handleSubmitQuestions = {
-    // fucntion
   };
-  const addQuestionField= {
+  // const handleSubmitQuestions = {
+  //   // fucntion
+  // };
+  const addQuestionField = {
     // add question flied
-  }
+  };
   return (
     <>
-      <div className="CompanyDetails">
-        <div>
-          <div>companyName</div>
-          <div>Description</div>
-          <div>HR Name</div>
+      <div className="bg-gray-50 py-4 flex">
+        <div className="CompanyDetails p-4 bg-gray-100 w-1/4">
+          <div className="flex flex-col items-center justify-center text-center">
+            <h2 className="text-2xl font-bold mb-4 font-serif">
+              MicroSoft Kuroku Pvt Limited
+            </h2>
+            <div className="mb-4">
+              <span className="italic font-bold">Description:</span>&nbsp;
+              Software Consulting Company Deals with all the trending
+              technologies
+            </div>
+            <div>
+              <span className="italic font-bold">HR Name:</span> &nbsp;
+              Hrushikesh Ambike
+            </div>
+          </div>
         </div>
-        <div>instructions</div>
+        <div className="flex w-2/4">
+          <div className="max-w-2xl mx-auto px-4 ">
+            <div className="flex justify-between items-center">
+              <h1 className="text-2xl font-bold text-gray-800">
+                New Interview
+              </h1>
+              <div className="text-right">
+                <button className="px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700">
+                  Save
+                </button>
+                <button className="px-4 py-2 rounded-md bg-red-600 text-white hover:bg-red-700 ml-2">
+                  Cancel
+                </button>
+              </div>
+            </div>
+            <div className="my-4">
+              <p className="text-gray-600">
+                Fill in the form below to schedule a new interview.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div className="InterviewDetails">
-        <Formik>
-          <Form onSubmit={handleSubmitInterview}>
-            <h1>Interview Details</h1>
-            <Field
-              type="text"
-              label="technologyName"
-              name="technologyName"
-              value={technologyName}
-              placeholder="Technology Name "
-              required
-              onChange={(Event) => {
-                setTechnologyName(Event.target.value);
-              }}
-            />
-            <Field
-              type="text"
-              label="noQuestions"
-              name="noQuestions"
-              value={noQuestions}
-              placeholder="Number of Questions "
-              required
-              onChange={(Event) => {
-                setNoQuestions(Event.target.value);
-              }}
-            />
+      <div className="py-6">
+        <div className="max-w-2xl mx-auto px-4">
+          <div className="bg-white shadow-md rounded-md p-4">
+            <h2 className="text-xl font-bold mb-4">Interview Details</h2>
+            <Formik>
+              <Form onSubmit={handleSubmitInterview}>
+                <div className="mb-4">
+                  <label
+                    htmlFor="technologyName"
+                    className="block text-gray-700 font-bold mb-2"
+                  >
+                    Technology Name
+                  </label>
+                  <Field
+                    type="text"
+                    id="technologyName"
+                    name="technologyName"
+                    value={technologyName}
+                    placeholder="Enter technology name"
+                    required
+                    onChange={(Event) => {
+                      setTechnologyName(Event.target.value);
+                    }}
+                    className="w-full px-3 py-2 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:border-blue-600"
+                  />
+                </div>
 
-            <Field
-              type="date"
-              label="interviewDate"
-              name="interviewDate"
-              value={interviewDate}
-              placeholder="Interview Date"
-              required
-              onChange={(Event) => {
-                setInterviewDate(Event.target.value);
-              }}
-            />
+                <div className="mb-4">
+                  <label
+                    htmlFor="noQuestions"
+                    className="block text-gray-700 font-bold mb-2"
+                  >
+                    Number of Questions
+                  </label>
+                  <Field
+                    type="number"
+                    id="noQuestions"
+                    name="noQuestions"
+                    value={noQuestions}
+                    placeholder="Enter number of questions"
+                    required
+                    onChange={(Event) => {
+                      setNoQuestions(Event.target.value);
+                    }}
+                    className="w-full px-3 py-2 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:border-blue-600"
+                  />
+                </div>
 
-            <Field
-              type="time"
-              label="interviewTime"
-              name="interviewTime"
-              value={interviewTime}
-              placeholder="Interview Time"
-              required
-              onChange={(Event) => {
-                setInterviewTime(Event.target.value);
-              }}
-            />
+                <div className="mb-4">
+                  <label
+                    htmlFor="interviewDate"
+                    className="block text-gray-700 font-bold mb-2"
+                  >
+                    Interview Date
+                  </label>
+                  <Field
+                    type="date"
+                    id="interviewDate"
+                    name="interviewDate"
+                    value={interviewDate}
+                    placeholder="Enter interview date"
+                    required
+                    onChange={(Event) => {
+                      setInterviewDate(Event.target.value);
+                    }}
+                    className="w-full px-3 py-2 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:border-blue-600"
+                  />
+                </div>
 
-            <Field
-              type="text"
-              label="interviewDuration"
-              name="interviewDuration"
-              value={interviewDuration}
-              placeholder="Interview Duration"
-              required
-              onChange={(Event) => {
-                setInterviewDuration(Event.target.value);
-              }}
-            />
-            <button type="submit">Schedule Interview</button>
-          </Form>
-        </Formik>
-      </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="interviewTime"
+                    className="block text-gray-700 font-bold mb-2"
+                  >
+                    Interview Time
+                  </label>
+                  <Field
+                    type="time"
+                    id="interviewTime"
+                    name="interviewTime"
+                    value={interviewTime}
+                    placeholder="Enter interview time"
+                    required
+                    onChange={(Event) => {
+                      setInterviewTime(Event.target.value);
+                    }}
+                    className="w-full px-3 py-2 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:border-blue-600"
+                  />
+                </div>
 
-      <div className="questions">
-      <Formik>
-          <Form onSubmit={handleSubmitQuestions}>
-            <h1>Interview Questions</h1>
-            <Field
-              type="text"
-              label="question"
-              name="question"
-              value={question}
-              placeholder="Enter question"
-              required
-              onChange={(Event) => {
-                setQuestion(Event.target.value);
-              }}
-            />
-            <Field
-              type="text"
-              label="answer"
-              name="answer"
-              value={answer}
-              placeholder="Enter Answer"
-              required
-              onChange={(Event) => {
-                setAnswer(Event.target.value);
-              }}
-            />
+                <div className="mb-4">
+                  <label
+                    htmlFor="interviewDuration"
+                    className="block text-gray-700 font-bold mb-2"
+                  >
+                    Interview Duration
+                  </label>
+                  <Field
+                    type="number"
+                    id="interviewDuration"
+                    name="interviewDuration"
+                    value={interviewDuration}
+                    placeholder="Enter interview duration in minutes"
+                    required
+                    onChange={(Event) => {
+                      setInterviewDuration(Event.target.value);
+                    }}
+                    className="w-full px-3 py-2 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:border-blue-600"
+                  />
+                </div>
 
-            <button type="button" onClick={addQuestionField}>+</button>
-            <button type="submit">submit</button>
-          </Form>
-        </Formik>  
+                <h2 className="text-xl font-bold mb-4">Questions</h2>
+                {[...Array(Number(noQuestions))].map((value, index) => {
+                  return (
+                    <div key={index}>
+                      <div className="mb-4">
+                        <label
+                          htmlFor="question"
+                          className="block text-gray-700 font-bold mb-2"
+                        >
+                          Question {index + 1}
+                        </label>
+                        <Field
+                          type="text"
+                          id={`question${index}`}
+                          name={`question${index}`}
+                          value={question}
+                          placeholder="Enter question"
+                          required
+                          onChange={(Event) => {
+                            setQuestion(Event.target.value);
+                          }}
+                          className="w-full px-3 py-2 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:border-blue-600"
+                        />
+                      </div>
 
+                      <div className="mb-4">
+                        <label
+                          htmlFor="answer"
+                          className="block text-gray-700 font-bold mb-2"
+                        >
+                          Answer {index + 1}
+                        </label>
+                        <Field
+                          type="text"
+                          id={`answer${index}`}
+                          name={`answer${index}`}
+                          value={answer}
+                          placeholder="Enter answer"
+                          required
+                          onChange={(Event) => {
+                            setAnswer(Event.target.value);
+                          }}
+                          className="w-full px-3 py-2 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:border-blue-600"
+                        />
+                      </div>
+                    </div>
+                  );
+                })}
+                <button
+                  type="submit"
+                  className="px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700"
+                >
+                  Save Interview
+                </button>
+              </Form>
+            </Formik>
+            <button
+              type="button"
+              onClick={addQuestionField}
+              className="mt-4 px-4 py-2 rounded-md bg-gray-600 text-white hover:bg-gray-700"
+            >
+              Add Question Field
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
