c0c32a550beeabe162ea66ea89f4373672a29f8d
hrushi01 <hrushikesh.ambike@gmail.com>
2023-03-05 14:42:56 +0530

    -

--- src/App.js
+++ src/App.js
@@ -3,28 +3,13 @@ import Login from "./login/Login";
 import Candidate from "./pages/Candidate";
 import { useState } from "react";
 import InterviewCam from "./components/InterviewCam";
-import { useRecordWebcam } from "react-record-webcam";
-import SpeechRecognition, {
-  useSpeechRecognition,
-} from "react-speech-recognition";
+
 import "./components/InterviewCam.css";
-import MicroPhoneTesting from "./components/MicroPhoneTesting";
 
 function App() {
   const [show, setShow] = useState(false);
   const [Itr, setItr] = useState(true);
 
-  const recordWebcam = useRecordWebcam({ frameRate: 60 });
-  const saveFile = async () => {
-    const blob = await recordWebcam.getRecording();
-  };
-
-  const {
-    transcript,
-    listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition,
-  } = useSpeechRecognition();
   return (
     <div>
       {show ? (
