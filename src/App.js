73b99eded01a3f47ba998e6f4c83d3051998818d
Hrushi01 <hrushikesh.ambike@gmail.com>
2023-04-12 00:55:39 +0530

    signup added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import AboutUs from "./pages/AboutUs";
 import LandingPg from "./pages/LandingPg";
 import ChatBot from "./ChatbotComponents/chatbot";
 import ResumeBuilder from "./ResumeBuilder/ResumeBuilder";
+import Signup from "./Login-Signup/signup/Signup";
 
 function App() {
   const [Display, setDisplay] = useState("home");
@@ -23,66 +24,67 @@ function App() {
     const CheckAlreadyLogin = cookies.get("SmartToken");
     if (CheckAlreadyLogin) {
       setIsLoggedIn(true);
-      console.log("0009",CheckAlreadyLogin)
+      console.log("0009", CheckAlreadyLogin);
     }
   }, [refresher]);
 
   return (
-    <div>
-      {Display === "home" ? (
-        <LandingPg setDisplay={setDisplay} />
-      ) : Display === "about" ? (
-        <AboutUs setDisplay={setDisplay} />
-      ) : Display === "chatbot" ? (
-        <ChatBot setDisplay={setDisplay} />
-      ) : Display === "resumebuilder" ? (
-        <ResumeBuilder setDisplay={setDisplay} />
-      ) : Display === "interview" ? (
-        isLogged ? (
-          OrganizationLog ? (
-            <WithLoginOrganization
-              OrganizationLog={OrganizationLog}
-              setOrganizationLog={setOrganizationLog}
-              candidateLog={candidateLog}
-              setCandidateLog={setCandidateLog}
-            />
-          ) : (
-            <WithLoginStudent
-              OrganizationLog={OrganizationLog}
-              setOrganizationLog={setOrganizationLog}
-              candidateLog={candidateLog}
-              setCandidateLog={setCandidateLog}
-              refresher={refresher}
-              setRefresher={setRefresher}
-            />
-          )
-        ) : (
-          <WithoutLogin
-            setDisplay={setDisplay}
-            show={show}
-            setShow={setShow}
-            OrganizationLog={OrganizationLog}
-            setOrganizationLog={setOrganizationLog}
-            candidateLog={candidateLog}
-            setCandidateLog={setCandidateLog}
-            isLogged={isLogged}
-            setIsLoggedIn={setIsLoggedIn}
-            refresher={refresher}
-            setRefresher={setRefresher}
-            setStatus={setStatus}
-            status={status}
-          />
-          // <WithLoginOrganization
-          //   OrganizationLog={OrganizationLog}
-          //   setOrganizationLog={setOrganizationLog}
-          //   candidateLog={candidateLog}
-          //   setCandidateLog={setCandidateLog}
-          // />
-        )
-      ) : (
-        <></>
-      )}
-    </div>
+    <Signup />
+    // <div>
+    //   {Display === "home" ? (
+    //     <LandingPg setDisplay={setDisplay} />
+    //   ) : Display === "about" ? (
+    //     <AboutUs setDisplay={setDisplay} />
+    //   ) : Display === "chatbot" ? (
+    //     <ChatBot setDisplay={setDisplay} />
+    //   ) : Display === "resumebuilder" ? (
+    //     <ResumeBuilder setDisplay={setDisplay} />
+    //   ) : Display === "interview" ? (
+    //     isLogged ? (
+    //       OrganizationLog ? (
+    //         <WithLoginOrganization
+    //           OrganizationLog={OrganizationLog}
+    //           setOrganizationLog={setOrganizationLog}
+    //           candidateLog={candidateLog}
+    //           setCandidateLog={setCandidateLog}
+    //         />
+    //       ) : (
+    //         <WithLoginStudent
+    //           OrganizationLog={OrganizationLog}
+    //           setOrganizationLog={setOrganizationLog}
+    //           candidateLog={candidateLog}
+    //           setCandidateLog={setCandidateLog}
+    //           refresher={refresher}
+    //           setRefresher={setRefresher}
+    //         />
+    //       )
+    //     ) : (
+    //       <WithoutLogin
+    //         setDisplay={setDisplay}
+    //         show={show}
+    //         setShow={setShow}
+    //         OrganizationLog={OrganizationLog}
+    //         setOrganizationLog={setOrganizationLog}
+    //         candidateLog={candidateLog}
+    //         setCandidateLog={setCandidateLog}
+    //         isLogged={isLogged}
+    //         setIsLoggedIn={setIsLoggedIn}
+    //         refresher={refresher}
+    //         setRefresher={setRefresher}
+    //         setStatus={setStatus}
+    //         status={status}
+    //       />
+    //       // <WithLoginOrganization
+    //       //   OrganizationLog={OrganizationLog}
+    //       //   setOrganizationLog={setOrganizationLog}
+    //       //   candidateLog={candidateLog}
+    //       //   setCandidateLog={setCandidateLog}
+    //       // />
+    //     )
+    //   ) : (
+    //     <></>
+    //   )}
+    // </div>
   );
 }
 
--- src/Login-Signup/signup/Signup.jsx
+++ src/Login-Signup/signup/Signup.jsx
@@ -1,25 +1,136 @@
-import React from "react";
-import Fields from "./SignupFields";
-import Navbar from "../login/Navbar";
-import Conference from "../images/conference.png";
+// import React from "react";
+// import Fields from "./SignupFields";
+// import Navbar from "../login/Navbar";
+// import Conference from "../images/conference.png";
 
-function Signup(props) {
-  const { show, setShow } = props;
+// function Signup(props) {
+//   const { show, setShow } = props;
+//   return (
+//     <div>
+//       <div className="">
+//         <Navbar />
+//       </div>
+//       <div className="flex">
+//         <div className="w-2/5">
+//           {/* <Fields show={show} setShow={setShow} /> */}
+//           hello
+//         </div>
+//         <div className="flex justify-center items-center p-28">
+//           <img src={Conference} alt="img" />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Signup;
+import React, { useState } from "react";
+
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // do something with formData, like sending it to a server
+  };
+
   return (
-    <div>
-      <div className="">
-        <Navbar />
-      </div>
-      <div className="flex">
-        <div className="w-2/5">
-          <Fields show={show} setShow={setShow} />
-        </div>
-        <div className="flex justify-center items-center p-28">
-          <img src={Conference} alt="img" />
-        </div>
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h2 className="text-2xl font-bold mb-8">Sign Up</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="name"
+            >
+              Name
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="confirmPassword"
+            >
+              Confirm Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              placeholder="Confirm Password"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
-}
+};
 
 export default Signup;
