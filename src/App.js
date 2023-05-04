725a33dde7d3d5f774eebc0e57ad161fdcb3fe78
MaleficScorpio <70068895+youreachedrahat@users.noreply.github.com>
2023-05-04 20:04:20 +0530

    dszvj

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { useState } from "react";
 import Features from "./pages/Features";
 import Home from "./pages/Home";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const [home, setHome] = useState(true);
-
+  const [isLogged, setIsLoggedIn] = useState(true);
   return (
     <>
       <div>{home ? <Home setHome={setHome} /> : <Features />}</div>
+      <BrowserRouter>
+      
+        {isLogged ? (
+          <WithLogin refresher={refresher} setRefresher={setRefresher}/>
+        ) : (
+          <WithoutLogin isLogged={isLogged} setIsLoggedIn={setIsLoggedIn} />
+        )}
+    
+    </BrowserRouter>
     </>
   );
 }
